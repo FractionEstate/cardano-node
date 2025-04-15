36e0ed06545bf2d920a5c8f20d9213db8c97f43b
Jürgen Nicklisch-Franken <jnf@arcor.de>
2025-04-15 14:49:14 +0200

    cardano-node: periodic ledger metrics tracer
    ledger metrics 700ms after slot start
    allow for not running periodic tracers

--- cardano-node/src/Cardano/Node/Tracing/Tracers/Resources.hs
+++ cardano-node/src/Cardano/Node/Tracing/Tracers/Resources.hs
@@ -11,17 +11,20 @@ import           Control.Concurrent.Async (async)
 import           Control.Monad (forM_, forever)
 import           Control.Monad.Class.MonadAsync (link)
 import           GHC.Conc (labelThread, myThreadId)
-
 import           "contra-tracer" Control.Tracer
 
 -- | Starts a background thread to periodically trace resource statistics.
 -- The thread reads resource stats and traces them using the given tracer.
 -- It is linked to the parent thread to ensure proper error propagation.
 startResourceTracer :: Tracer IO ResourceStats -> Int -> IO ()
+startResourceTracer
+  :: Tracer IO ResourceStats
+  -> Int
+  -> IO ()
+startResourceTracer _ 0 = pure ()
 startResourceTracer tracer delayMilliseconds = do
-  thread <- async resourceThread
-  -- Link the thread to the parent to propagate exceptions properly.
-  link thread
+    as <- async resourceThread
+    link as
   where
     -- | The background thread that periodically traces resource stats.
     resourceThread :: IO ()
