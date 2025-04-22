790ae17a805b24cc160ac44cc6f737e86608a40e
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2025-04-22 19:12:19 +0200

    sadf

--- cardano-testnet/test/cardano-testnet-test/Cardano/Testnet/Test/Api/TxSupplementalDatum.hs
+++ cardano-testnet/test/cardano-testnet-test/Cardano/Testnet/Test/Api/TxSupplementalDatum.hs
@@ -11,6 +11,7 @@ module Cardano.Testnet.Test.Api.TxSupplementalDatum
 where
 
 import           Cardano.Api
+import           Cardano.Api.Experimental
 import qualified Cardano.Api.Ledger as L
 import qualified Cardano.Api.Network as Net
 import qualified Cardano.Api.Network as Net.Tx
@@ -26,13 +27,14 @@ import qualified Data.Map.Strict as M
 import           Data.Proxy
 import           Data.Set (Set)
 import           GHC.Exts (IsList (..))
+import           GHC.Stack
 import           Lens.Micro
 
 import           Testnet.Components.Query
 import           Testnet.Property.Util (integrationRetryWorkspace)
 import           Testnet.Types
 
-import           Hedgehog (Property, (===))
+import           Hedgehog
 import qualified Hedgehog as H
 import qualified Hedgehog.Extras.Test.Base as H
 import qualified Hedgehog.Extras.Test.TestWatchdog as H
@@ -82,15 +84,22 @@ hprop_tx_supp_datum = integrationRetryWorkspace 2 "api-tx-supp-dat" $ \tempAbsBa
       executeLocalStateQueryExpr connectionInfo Net.VolatileTip $
         fmap toLedgerEpochInfo <$> queryEraHistory
 
-  let scriptData1 = unsafeHashableScriptData $ ScriptDataBytes "CAFEBABE"
-      scriptData2 = unsafeHashableScriptData $ ScriptDataBytes "DEADBEEF"
-      txDatum1 =
+  let scriptData1 = unsafeHashableScriptData $ ScriptDataBytes "HASH_1" -- hash
+      scriptData2 = unsafeHashableScriptData $ ScriptDataBytes "INLINE_1" -- inline
+      scriptData3 = unsafeHashableScriptData $ ScriptDataBytes "SUPPLEMENTAL_1" -- supplemental
+  -- 4e62677c3b9f3b247502efe39a85aadcc2f2d3a32aec544d62175ed86c57fe9b
+  H.noteShow_ $ hashScriptDataBytes scriptData1
+  -- c93bae5c7cb737e16eb224d1884e7fbe14dc038caf1b511e34a43e67d3eb9f63
+  H.noteShow_ $ hashScriptDataBytes scriptData2
+  -- 74ea77567269646d49e072bd83e701ff7e43574522ad90833bcfa554658c65bb
+  H.noteShow_ $ hashScriptDataBytes scriptData3
+  let txDatum1 =
         TxOutDatumHash
           (convert beo)
           (hashScriptDataBytes scriptData1)
-      txDatum2 = TxOutDatumInline (convert ceo) scriptData2
+      txDatum2 = TxOutDatumInline beo scriptData2
+      txDatum3 = TxOutSupplementalDatum (convert beo) scriptData3
 
-  -- Build a first transaction with txout supplemental data
   tx1Utxo <- do
     txIn <- findLargestUtxoForPaymentKey epochStateView sbe wallet0
 
@@ -99,6 +108,7 @@ hprop_tx_supp_datum = integrationRetryWorkspace 2 "api-tx-supp-dat" $ \tempAbsBa
         txOuts =
           [ TxOut addr1 txOutValue txDatum1 ReferenceScriptNone
           , TxOut addr1 txOutValue txDatum2 ReferenceScriptNone
+          , TxOut addr1 txOutValue txDatum3 ReferenceScriptNone
           ]
 
         -- build a transaction
@@ -110,7 +120,8 @@ hprop_tx_supp_datum = integrationRetryWorkspace 2 "api-tx-supp-dat" $ \tempAbsBa
 
     utxo <- UTxO <$> findAllUtxos epochStateView sbe
 
-    BalancedTxBody _ txBody _ fee <-
+
+    BalancedTxBody _ txBody@(ShelleyTxBody _ lbody _ (TxBodyScriptData _ (L.TxDats' datums) _) _ _) _ fee <-
       H.leftFail $
         makeTransactionBodyAutoBalance
           sbe
@@ -126,12 +137,21 @@ hprop_tx_supp_datum = integrationRetryWorkspace 2 "api-tx-supp-dat" $ \tempAbsBa
           Nothing -- keys override
     H.noteShow_ fee
 
+    H.noteShowPretty_ lbody
+
+    lbody ^. L.scriptIntegrityHashTxBodyL /== L.SNothing
+
+    let bodyScriptData = fromList . map fromAlonzoData $ M.elems datums :: Set HashableScriptData
+
+    -- Only supplemental datum are included here
+    [ scriptData3 ] === bodyScriptData
+
     let tx = signShelleyTransaction sbe txBody [wit0]
     txId <- H.noteShow . getTxId $ getTxBody tx
 
-    H.evalIO (submitTxToNodeLocal connectionInfo (TxInMode sbe tx)) >>= \case
-      Net.Tx.SubmitFail reason -> H.noteShow_ reason >> H.failure
-      Net.Tx.SubmitSuccess -> H.success
+    H.noteShowPretty_ tx
+
+    submitTx sbe connectionInfo tx
 
     -- wait till transaction gets included in the block
     _ <- waitForBlocks epochStateView 1
@@ -139,49 +159,56 @@ hprop_tx_supp_datum = integrationRetryWorkspace 2 "api-tx-supp-dat" $ \tempAbsBa
     -- test if it's in UTxO set
     utxo1 <- findAllUtxos epochStateView sbe
     let txUtxo = M.filterWithKey (\(TxIn txId' _) _ -> txId == txId') utxo1
-    3 === length txUtxo
+    -- H.noteShowPretty_ txUtxo
+    (length txOuts + 1) === length txUtxo
 
     let chainTxOuts =
           reverse
             . drop 1
             . reverse
-            . map (fromCtxUTxOTxOut . snd)
+            . map snd
             . toList
             $ M.filterWithKey (\(TxIn txId' _) _ -> txId == txId') utxo1
 
-    txOuts === chainTxOuts
+    (toCtxUTxOTxOut <$> txOuts) === chainTxOuts
 
     pure txUtxo
 
   do
-    [(txIn1, _)] <- pure $ filter (\(_, TxOut _ _ datum _) -> datum == txDatum1) $ toList tx1Utxo
-    [(txIn2, _)] <- pure $ filter (\(_, TxOut _ _ datum _) -> datum == txDatum2) $ toList tx1Utxo
+    let txDatum3' = TxOutDatumHash (convert beo) (hashScriptDataBytes scriptData3)
+    [(txIn1, _)] <- H.noteShowPretty $ filter (\(_, TxOut _ _ datum _) -> datum == txDatum1) $ toList tx1Utxo
+    [(txIn2, _)] <- H.noteShowPretty $ filter (\(_, TxOut _ _ datum _) -> datum == txDatum2) $ toList tx1Utxo
+    [(txIn3, _)] <- H.noteShowPretty $ filter (\(_, TxOut _ _ datum _) -> datum == txDatum3') $ toList tx1Utxo
 
-    let scriptData3 = unsafeHashableScriptData $ ScriptDataBytes "C0FFEE"
-        txDatum = TxOutDatumInline (convert ceo) scriptData3
+    let scriptData4 = unsafeHashableScriptData $ ScriptDataBytes "SUPPLEMENTAL_2"
+        txDatum = TxOutSupplementalDatum (convert beo) scriptData4
         txOutValue = lovelaceToTxOutValue sbe 99_999_500
         txOut = TxOut addr0 txOutValue txDatum ReferenceScriptNone
+        txInsReference = TxInsReference beo [txIn1, txIn3] $ pure [scriptData1, scriptData2, scriptData3, scriptData4]
 
     let content =
           defaultTxBodyContent sbe
-            & setTxIns [(txIn1, pure $ KeyWitness KeyWitnessForSpending)]
-            & setTxInsReference (TxInsReference beo [txIn2])
+            & setTxIns [(txIn2, pure $ KeyWitness KeyWitnessForSpending)]
+            & setTxInsReference txInsReference
             & setTxFee (TxFeeExplicit sbe 500)
             & setTxOuts [txOut]
+            & setTxProtocolParams (pure $ pure pparams)
 
-    txBody@(ShelleyTxBody _ _ _ (TxBodyScriptData _ (L.TxDats' datums) _) _ _) <-
-      H.leftFail $ createTransactionBody sbe content
+    txBody@(ShelleyTxBody _ lbody _ (TxBodyScriptData _ (L.TxDats' datums) _) _ _) <-
+      H.leftFail $ createTransactionBody sbe (UTxO tx1Utxo) content
+
     let bodyScriptData = fromList . map fromAlonzoData $ M.elems datums :: Set HashableScriptData
-    -- TODO why bodyScriptData is empty here?
-    [scriptData1, scriptData2, scriptData3] === bodyScriptData
+    [scriptData1, scriptData3, scriptData4] === bodyScriptData
+
+    H.noteShowPretty_ txBody
+
+    lbody ^. L.scriptIntegrityHashTxBodyL /== L.SNothing
 
     let tx = signShelleyTransaction sbe txBody [wit1]
-    -- H.noteShowPretty_ tx
+    H.noteShowPretty_ tx
     txId <- H.noteShow . getTxId $ getTxBody tx
 
-    H.evalIO (submitTxToNodeLocal connectionInfo (TxInMode sbe tx)) >>= \case
-      Net.Tx.SubmitFail reason -> H.noteShow_ reason >> H.failure
-      Net.Tx.SubmitSuccess -> H.success
+    submitTx sbe connectionInfo tx
 
     -- wait till transaction gets included in the block
     _ <- waitForBlocks epochStateView 1
@@ -189,6 +216,20 @@ hprop_tx_supp_datum = integrationRetryWorkspace 2 "api-tx-supp-dat" $ \tempAbsBa
     -- test if it's in UTxO set
     utxo1 <- findAllUtxos epochStateView sbe
     let txUtxo = M.filterWithKey (\(TxIn txId' _) _ -> txId == txId') utxo1
-    [txOut] === M.elems txUtxo
+    [toCtxUTxOTxOut txOut] === M.elems txUtxo
 
   H.failure
+
+submitTx
+  :: MonadTest m
+  => MonadIO m
+  => HasCallStack
+  => ShelleyBasedEra era
+  -> LocalNodeConnectInfo
+  -> Tx era
+  -> m ()
+submitTx sbe connectionInfo tx =
+  withFrozenCallStack $
+    H.evalIO (submitTxToNodeLocal connectionInfo (TxInMode sbe tx)) >>= \case
+      Net.Tx.SubmitFail reason -> H.noteShowPretty_ reason >> H.failure
+      Net.Tx.SubmitSuccess -> H.success
